b78ed9827bebbb8a1517d0bedd56af3699c10468
HaruhikoHasegawa <80626341+halpaka@users.noreply.github.com>
2024-05-27 16:29:25 +0900

    Add files via upload

--- front.js
+++ front.js
@@ -15,6 +15,24 @@ function adjustTextareaHeight() {
     }
 }
 
+//字幕の表示・非表示機能
+function displaySubtitles(st){
+    const tf = st.dataset.TF;
+
+    if(tf == "true"){
+        st.style.color = '#F5EDED';     //背景に同化
+        st.style.userSelect = "none";   //選択の拒否
+        st.dataset.TF = "false";        //フラグの修正
+    }
+    else{
+        st.style.color = 'black';       //文字表示
+        st.style.userSelect = "auto";   //選択の許可
+        st.dataset.TF = "true";
+    }
+
+}
+
 // 初期化
 window.addEventListener('load', adjustTextareaHeight);
+//ウィンドウサイズの変更時
 window.addEventListener('resize', adjustTextareaHeight);
