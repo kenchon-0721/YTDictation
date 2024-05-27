27d97d9d93005f9b26886d6e4d912a24c2c2cf23
HaruhikoHasegawa <80626341+halpaka@users.noreply.github.com>
2024-05-27 11:44:12 +0900

    Add files via upload

--- front.js
+++ front.js
@@ -3,14 +3,16 @@ function adjustTextareaHeight() {
     const table = document.getElementById('dictationTable');
     const rows = table.getElementsByTagName('tr');
 
-for (let i = 0; i < rows.length; i++) {
-    const row = rows[i];
-    const cells = row.getElementsByTagName('td');
-    const textarea = cells[1].querySelector('textarea');
-    const rowHeight = row.scrollHeight;
+    for (let i = 1; i < rows.length; i++) {
+        const row = rows[i];
+        const cells = row.getElementsByTagName('td');
+        const textarea = cells[1].querySelector('textarea');
 
-    textarea.style.height = rowHeight + 'px';
-}
+        textarea.style.height = 'auto';
+        const rowHeight = row.scrollHeight;
+        
+        textarea.style.height = rowHeight + 'px';
+    }
 }
 
 // 初期化
