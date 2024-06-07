e402dd9945b5863dcac4146cffa907b62870b14a
HaruhikoHasegawa <80626341+halpaka@users.noreply.github.com>
2024-06-08 00:47:28 +0900

    Add files via upload

--- main/scripts/front.js
+++ main/scripts/front.js
@@ -37,11 +37,35 @@ window.addEventListener('load', adjustTextareaHeight);
 //ウィンドウサイズの変更時
 window.addEventListener('resize', adjustTextareaHeight);
 
+//前の表の削除
+function deletingTable(){
+    const deleteTable = Array.from(document.getElementsByClassName('captionLine'));
+    if(deleteTable != null){
+        deleteTable.forEach(deleteLine => deleteLine.remove());
+    }
+}
+
+//表の作成
+function makingTable(){
+    const tableContents = document.getElementById('dictationTable');
+    let insertElement;
+
+    for (let i = 0; i < caption.list.length; i++){
+        insertElement = '<tr class="captionLine">\n<td class="col0">\n<button class="rewind">'
+        insertElement += caption.list[i].time + '</button>\n</td>\n<td class="col1">\n<textarea class="textDictation"></textarea>\n</td>\n';
+        insertElement += '<td class="col2" data-TF="false" ondblclick="displaySubtitles(this)">' + caption.list[i].caption + '\n</td>\n</tr>\n';
+
+        tableContents.insertAdjacentHTML('beforeend', insertElement);
+    }
+}
+
 
 //字幕の準備が整った際に呼び出される関数
 //caption.list により、字幕情報が取得できます。
 function onCaptionReady(){
     let caption_list = caption.list;
+    deletingTable();
+    makingTable();
     caption_list.forEach(elm => {
         console.log(`${elm.time}    ${elm.caption}`);
     });
